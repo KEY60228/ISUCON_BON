9a400c36b3447b023a192fd0041cbb813a650049
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-09-01 20:20:33 +0900

    timeとcontextによるループのパターン - 1

--- main.go
+++ main.go
@@ -7,24 +7,18 @@ import (
 )
 
 func main() {
-	ctxMain := context.Background()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
-	go func() {
-		ctxTimeout, cancelTimeout := context.WithTimeout(ctxMain, 5*time.Second)
-		defer cancelTimeout()
-		<-ctxTimeout.Done()
-		fmt.Println("timeout!")
-	}()
+L:
+	for i := 0; ; i++ {
+		fmt.Printf("loop %d\n", i)
 
-	go func() {
-		ctxDeadline, cancelDeadline := context.WithDeadline(ctxMain, time.Now().Add(3*time.Second))
-		defer cancelDeadline()
-		<-ctxDeadline.Done()
-		fmt.Println("deadline!")
-	}()
-
-	for i := 0; i < 10; i++ {
-		fmt.Printf("%d sec...\n", i)
-		time.Sleep(1 * time.Second)
+		select {
+		case <-ctx.Done():
+			break L
+		default:
+			time.Sleep(1 * time.Second)
+		}
 	}
 }
