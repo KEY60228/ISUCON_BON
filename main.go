044385c116b1a9d078a8b1d4433d02af9830ae40
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-09-01 20:06:01 +0900

    context.WithCancelによるcontext.Contextの中断

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 func main() {
-	ctx := context.Background()
-	ExampleContextFunc(ctx)
-}
+	ctxParent, cancelParent := context.WithCancel(context.Background())
+	defer cancelParent()
 
-func ExampleContextFunc(ctx context.Context) {}
+	ctxChild, cancelChild := context.WithCancel(ctxParent)
+	defer cancelChild()
+
+	// 親contextの中断は子contextにも伝播する
+	cancelParent()
+	// 子contextの中断は親contextには伝播しない
+	// cancelChild()
+
+	fmt.Printf("parent.Err is %v\n", ctxParent.Err())
+	fmt.Printf("child.Err is %v\n", ctxChild.Err())
+}
