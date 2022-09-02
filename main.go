0bcc16ca410321bd1ccf86ab8dd41ff9d9837606
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-09-02 22:09:19 +0900

    timeとcontextによるループのパターン - 3

--- main.go
+++ main.go
@@ -10,14 +10,45 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-L:
-	for i := 0; ; i++ {
-		fmt.Printf("loop %d\n", i)
+	go LoopWithBefore(ctx)
+	go LoopWithAfter(ctx)
+
+	<-ctx.Done()
+}
+
+func LoopWithBefore(ctx context.Context) {
+	beforeLoop := time.Now()
+	for {
+		loopTimer := time.After(3 * time.Second)
+
+		HeavyProcess(ctx, "BEFORE")
 
 		select {
 		case <-ctx.Done():
-			break L
-		case <-time.After(1 * time.Second):
+			return
+		case <-loopTimer:
+			fmt.Printf("[Before] loop duration: %.2fs\n", time.Now().Sub(beforeLoop).Seconds())
+			beforeLoop = time.Now()
 		}
 	}
+}
+
+func LoopWithAfter(ctx context.Context) {
+	beforeLoop := time.Now()
+	for {
+		HeavyProcess(ctx, "AFTER")
+
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(3 * time.Second):
+			fmt.Printf("[AFTER] loop duration: %.2fs\n", time.Now().Sub(beforeLoop).Seconds())
+			beforeLoop = time.Now()
+		}
+	}
+}
+
+func HeavyProcess(ctx context.Context, pattern string) {
+	fmt.Printf("[%s] Heavy Process\n", pattern)
+	time.Sleep(1*time.Second + 500*time.Millisecond)
 }
