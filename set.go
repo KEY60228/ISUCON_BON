8d839b38e39ecc8986812db7cec4430d892fb867
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-09-04 13:28:22 +0900

    初期データのロード

--- set.go
+++ set.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"os"
 	"sync"
 	"time"
 )
@@ -73,4 +76,27 @@ func (s *Set[T]) Add(model T) bool {
 	s.dict[id] = model
 
 	return true
+}
+
+func (s *Set[T]) LoadJSON(jsonFile string) error {
+	file, err := os.Open(jsonFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	models := []T{}
+
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&models); err != nil {
+		return err
+	}
+
+	for _, model := range models {
+		if !s.Add(model) {
+			return fmt.Errorf("unexpected error on dump loading: %v", model)
+		}
+	}
+
+	return nil
 }
