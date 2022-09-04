45fcf053f0f08e19fabc2140c2ada0934e09a66c
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2022-09-04 14:59:18 +0900

    Userにagent.Agentを持たせる

--- model.go
+++ model.go
@@ -1,14 +1,44 @@
 package main
 
-import "time"
+import (
+	"sync"
+	"time"
+
+	"github.com/isucon/isucandar/agent"
+)
 
 type User struct {
+	mu sync.RWMutex
+
 	ID          int       `json:"id"`
 	AccountName string    `json:"account_name"`
 	Password    string    `json:"password"`
 	Authority   int       `json:"authority"`
 	DeleteFlag  int       `json:"del_flg"`
 	CreatedAt   time.Time `json:"created_at"`
+
+	Agent *agent.Agent
+}
+
+func (m *User) GetAgent(o Option) (*agent.Agent, error) {
+	m.mu.RLock()
+	a := m.Agent
+	m.mu.RUnlock()
+
+	if a != nil {
+		return a, nil
+	}
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	a, err := o.NewAgent(false)
+	if err != nil {
+		return nil, err
+	}
+	m.Agent = a
+
+	return a, nil
 }
 
 type Post struct {
